98331cf889ea501155b2d9c9a8a555de58e39874
Arthur Moreira <arthurmcs96@gmail.com>
2022-10-13 21:02:37 -0300

    cria a logica de validar se a assinatura está ativa ou não

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -12,6 +12,46 @@ export const authOptions = {
   ],
 
   callbacks: {
+    async session({ session }){
+      try {
+
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index('user_by_email'),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(
+                q.Index('subscription_by_status'),
+                q.Casefold("active")
+              )
+            ]
+            )
+          )
+        )
+  
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription
+        }
+      } catch(err) {
+          console.log(err);
+          return {
+            ...session,
+            activeSubscription: null,
+          }
+      }
+    },
+    
     async signIn({ user }) {
       const { email } = user;
 
